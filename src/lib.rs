ba23c92f8b54bcd1d89c3fab67e511fc2af68958
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-11-18 18:57:50 -0500

    helper function to get user input

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+pub mod helpers {
+    use std::io::{self, Write};
+
+    pub fn user_input(prompt: Option<&str>) -> String {
+        loop {
+            if let Some(msg) = prompt {
+                print!("{msg}");
+                io::stdout().flush().unwrap();
+            }
+            
+            let mut input = String::new();
+            if std::io::stdin().read_line(&mut input).is_err() {
+                println!("could not read from the stdin, please try again");
+                continue;
+            }
+    
+            return input.trim().to_string();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works(){
+        assert_eq!(1+1, 2);
+    }
+}
